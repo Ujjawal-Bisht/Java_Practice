659b8d971958e15fc55c8dce5b9db7f593a7f1aa
Ujjawal-Bisht <ujjawal.bisht@outlook.com>
2026-07-10 08:57:54 +0530

    text to word conv completed.

--- src/convertorProject/Main.java
+++ src/convertorProject/Main.java
@@ -1,19 +1,27 @@
 package convertorProject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 	
-	public static String readData(String path) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(path));
-	    StringBuilder data = new StringBuilder();
-	    String s;
-	    while ((s = br.readLine()) != null) {
+	public static String readData(String path) throws IOException {	
+		
+		// This method will read data from .txt and .doc files.
+		// return complete file data in string format.
+		
+		File f = new File(path) ;
+		FileReader fr = new FileReader(f) ;
+		BufferedReader br = new BufferedReader(fr) ;
+		StringBuilder data = new StringBuilder() ;
+		
+		String s ;
+		while ((s = br.readLine()) != null) {
 	        data.append(s).append("\n");
 	    }
 	    br.close();
@@ -21,17 +29,40 @@ public class Main {
 		
 	}
 	
-	public static void convertToWord(String path, String res)throws IOException{
-		String data = readData(path) ;
-		File f = new File(res) ;
-		PrintWriter pw = new PrintWriter(f) ;
+	public static int writedata(String path, String data) throws IOException{
 		
-		while(!data.isEmpty()) {
-			pw.println(data) ;
+		// This method will write data to .doc and .txt file
+		// A new file will be created, if already present it will throw error.
+		
+		File f = new File(path) ;
+		if(f.exists()) {
+			System.err.println("File Already Exist!\nTry another file name.");
+			return -1;
 		}
-		pw.flush() ;
-		pw.close();
+		if(data == null) {
+			System.err.println("Cannot write in file due to no/corrupted data.");
+			return -1;
+		}
+		FileWriter fr = new FileWriter(f) ;
+		BufferedWriter bw = new BufferedWriter(fr) ;
 		
+		bw.write(data) ;
+		bw.flush() ;
+		bw.close() ;
+		return 0 ;
+		
+	}
+	
+	public static void convert(String path, String res)throws IOException{
+		// This method will convert: text file -> word doc && doc file ->text file
+		
+		String data = readData(path) ;
+		int x =writedata(res, data);
+		
+		if(x == 0) {
+			System.out.println("File Converted Successfully!!!");
+		}
+		return ;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -51,25 +82,26 @@ public class Main {
 		
 		switch(ch) {
 			case 1 ->{
-				
+				System.out.println("This feature will available soon....");
 			}
 			case 2 ->{
-				
+				System.out.println("This feature will available soon....");
 			}
 			case 3 ->{
-				convertToWord(path, res) ;
-				System.out.println("File Converted Successfully!!!");
+				convert(path, res) ;
 			}
 			case 4 ->{
-				
+				convert(path, res) ;
 			}
 			case 5 ->{
-				
+				System.out.println("This feature will available soon....");
 			}
 			case 6 ->{
-				
+				System.out.println("This feature will available soon....");
 			}
-			default -> {}
+			default -> {
+				System.err.println("Please Enter a valid choice!3");
+			}
 		}
 		sc.close();
 	}
